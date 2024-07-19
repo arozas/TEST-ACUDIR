5ab26189e2184da855d91abe5116e0ed1fe40e91
Alejandro Rozas <ecosistemas10@cnp.com.ar>
2024-07-18 21:16:57 -0300

    feature/test-AlejandroRozas - Definición de dbcontext.

--- Acudir.Test.Apis/Program.cs
+++ Acudir.Test.Apis/Program.cs
@@ -1,9 +1,12 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 var dataTest = System.IO.File.ReadAllText(@"Test.json");
 
---
+++ Acudir.Test.Infrastructure/Data/PersonContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using Acudir.Test.Core.Entities;
+
+namespace Acudir.Test.Infrastructure.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("InMemoryDb");
+        }
+
+        public override int SaveChanges()
+        {
+            var entities = Persons.ToList();
+            var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+            var json = JsonSerializer.Serialize(entities, jsonOptions);
+            File.WriteAllText("./Data/SeedData/Test.json", json);
+            return base.SaveChanges();
+        }
+
+        public void LoadData()
+        {
+            if (File.Exists("./Data/SeedData/Test.json"))
+            {
+                var json = File.ReadAllText("./Data/SeedData/Test.json");
+                var entities = JsonSerializer.Deserialize<List<Person>>(json);
+                Persons.AddRange(entities);
+                base.SaveChanges();
+            }
+        }
+    }
+}
